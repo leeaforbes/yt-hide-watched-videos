05b94ea0d47ba33ecac0884de261c474bd302109
Lee Forbes <fillthatinfo@gmail.com>
2025-03-13 19:18:51 -0500

    all button does nothing

--- popup/popup.js
+++ popup/popup.js
@@ -1,19 +1,33 @@
-document.getElementById("watch-threshold").addEventListener("click", saveWatchThreshold);
-document.getElementById("watch-threshold").addEventListener("mousemove", displayWatchThreshold);
-let toggle = document.getElementById("watch-threshold")
+let allCloseToggle = document.getElementById("all-on")
+let thresholdSlider = document.getElementById("watch-threshold")
 
+allCloseToggle.addEventListener("click", saveAllOn);
+thresholdSlider.addEventListener("click", saveWatchThreshold);
+thresholdSlider.addEventListener("mousemove", displayWatchThreshold);
+
+// all on
+chrome.storage.sync.get('allOn', function(data) {
+    let toggle = document.getElementById("auto-close-checkbox")
+    toggle.checked = data.allOn
+});
+
+function saveAllOn() {
+    let toggle = document.getElementById("auto-close-checkbox")
+    chrome.storage.sync.set({ allOn: toggle.checked });
+}
+
+// watch threshold
 chrome.storage.sync.get('watchThreshold', function(data) {
-    toggle.value = data.watchThreshold
+    thresholdSlider.value = data.watchThreshold
     displayWatchThreshold()
 });
 
 function displayWatchThreshold() {
-    document.getElementById("threshold-value").innerHTML = "<b>" + toggle.value + "%</b>"
+    console.log(thresholdSlider.value)
+    document.getElementById("threshold-value").innerHTML = "<b>" + thresholdSlider.value + "%</b>"
 }
 
 function saveWatchThreshold() {
-    // let toggle = document.getElementById("watch-threshold")
-    chrome.storage.sync.set({ watchThreshold: toggle.value });
+    chrome.storage.sync.set({ watchThreshold: thresholdSlider.value });
     displayWatchThreshold()
-    // console.log(toggle.value)
 }
